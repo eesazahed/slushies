1064d3cb18a498aeea54a9784198523cefeb6a02
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-21 16:56:29 -0500

    adding in green background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,100 +15,105 @@
   <link rel="preload" as="image" href="/images/slushies.png">
 </svelte:head>
 
-<header class="relative p-8 min-h-96 text-center bg-gradient-to-t from-lime-500 to-emerald-400 flex flex-col items-center justify-center overflow-hidden">
-  <div
-    id="bg-overlay"
-    class="absolute inset-0 bg-center bg-cover pointer-events-none"
-    style="opacity: {bgToggled ? 1 : 0}; background-image: url({bgImageUrl});">
-  </div>
 
-  <div class="text-white relative z-10 my-24">
-    <h1 on:click={toggleHeaderBg} class="cursor-pointer text-8xl font-bold mb-8">Slushies</h1>
-    <h2 class="text-4xl font-bold mb-8">A Flask YSWS</h2>
-    <p class="text-lg max-w-3xl mx-auto">Build something cool with Flask, ship your project, and get rewarded!</p>
-    <div class="mt-24 grid grid-cols-2 gap-6">
-      <a href="#" target="_blank" rel="noopener noreferrer" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">Submit YSWS →</a>
-      <a href="#" target="_blank" rel="noopener noreferrer" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">Join Slack →</a>
-      <a href="#requirements" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">Requirements →</a>
-      <a href="#faq" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">FAQ →</a>
+<div class="md:bg-green-200 md:pb-24">
+  <header class="relative p-8 min-h-96 text-center bg-gradient-to-t from-lime-500 to-emerald-400 flex flex-col items-center justify-center overflow-hidden">
+    <div
+      id="bg-overlay"
+      class="absolute inset-0 bg-center bg-cover pointer-events-none"
+      style="opacity: {bgToggled ? 1 : 0}; background-image: url({bgImageUrl});">
     </div>
-  </div>
-</header>
 
-<main class="max-w-5xl mx-auto px-24 py-24 bg-white">
-  <section class="mb-16 grid grid-cols-1 md:grid-cols-2 gap-10 text-center">
-    <div class="p-12 bg-green-100 border border-5 border-green-300 rounded-xl">
-      <h2 class="text-3xl font-semibold mb-4">You Ship</h2>
-      <p class="mb-2 text-base font-medium">A Flask web app with at least 3 routes. Host it on Nest.</p>
-      <p class="text-sm">(don't worry I'll make some tutorials)</p>
+    <div class="text-white relative z-10 my-16">
+      <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <h1 on:click={toggleHeaderBg} class="cursor-pointer text-6xl md:text-8xl font-bold mb-8">Slushies</h1>
+      <h2 class="text-4xl font-bold mb-8">A Flask YSWS</h2>
+      <p class="text-lg max-w-3xl mx-auto">Build something cool with Flask, ship your project, and get rewarded!</p>
+      <div class="mt-16 grid grid-cols-2 gap-6">
+        <a href="#" target="_blank" rel="noopener noreferrer" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">Submit YSWS</a>
+        <a href="#" target="_blank" rel="noopener noreferrer" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">Join Slack</a>
+        <a href="#requirements" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">Requirements</a>
+        <a href="#faq" class="inline-block px-9 py-4 rounded-xl text-white bg-cyan-600 hover:bg-cyan-500 transition">FAQ</a>
+      </div>
     </div>
-    <div class="p-12 bg-green-100 border border-5 border-green-300 rounded-xl">
-      <h2 class="text-3xl font-semibold mb-4">We Ship</h2>
-      <p class="mb-2 text-base font-medium">$5 reward for food vendors</p>
-      <p class="text-sm">(Get $10 if you implement CRUD with SQLite, CSV, Postgres, or similar)</p>
-    </div>
-  </section>
+  </header>
 
-  <section class="pt-8 mb-24 max-w-3xl mx-auto" id="requirements">
-    <h2 class="text-3xl font-semibold mb-8">Requirements</h2>
-    <ul class="list-disc list-inside space-y-3 text-base">
-      <li>Build a Flask web app with at least 3 routes</li>
-      <li>Commit your code frequently on GitHub</li>
-      <li>Describe your project in a README.md file and include some screenshots</li>
-      <li>Host the app on Nest (deployment tutorial provided)</li>
-      <li>Implement basic CRUD functionality using SQLite, CSV, Postgres, or similar for higher rewards</li>
-      <li>Your project should be functional, somewhat styled, and easy to use</li>
-    </ul>
-    <p class="text-xl font-semibold mt-12">Additionally, please note that:</p>
-      <ul class="list-disc list-inside mt-6  space-y-3 text-base">
-      <li>You must be in high school or middle school</li>
-      <li>You started working on this project recently. No submitting old projects.</li>
-      <li>You haven't gotten a Slushies grant before. If you have, maybe try some other <a href="https://ysws.hackclub.com" target="_blank" rel="noopener noreferrer" class="underline">YSWS</a></li>
-      <li>This is a beginner YSWS, I don't want to see any AI made projects</li>
-      <li>If you do use AI a bit for inspiration/debugging, make sure to include it in your README</li>
-    </ul>
-  </section>
+  <main class="max-w-4xl md:mt-24 mx-auto px-16 py-16 bg-white rounded-4xl">
+    <section class="mb-16 grid grid-cols-1 md:grid-cols-2 gap-10 text-center">
+      <div class="p-12 bg-green-100 border border-5 border-green-300 rounded-xl">
+        <h2 class="text-3xl font-semibold mb-4">You Ship</h2>
+        <p class="mb-2 text-base font-medium">A Flask web app with at least 3 routes. Host it on Nest.</p>
+        <p class="text-sm">(don't worry I'll make some tutorials)</p>
+      </div>
+      <div class="p-12 bg-green-100 border border-5 border-green-300 rounded-xl">
+        <h2 class="text-3xl font-semibold mb-4">We Ship</h2>
+        <p class="mb-2 text-base font-medium">$5 reward for food vendors</p>
+        <p class="text-sm">(Get $10 if you implement CRUD with SQLite, CSV, Postgres, or similar)</p>
+      </div>
+    </section>
 
-  <section class="pt-8 mb-24 max-w-3xl mx-auto" id="faq">
-    <h2 class="text-3xl font-semibold mb-8">Frequently Asked Questions</h2>
-    <dl class="space-y-8">
-      <div>
-        <dt class="font-semibold text-lg">Where can I ask for help?</dt>
-        <dd class="mt-1 text-base">
-          Join <a href="https://hackclub.slack.com" target="_blank" rel="noopener noreferrer" class="underline">#slushies</a> on Hack Club Slack or email <a href="mailto:eesa@hackclub.app" class="underline">eesa@hackclub.app</a>
-        </dd>
-      </div>
-      <div>
-        <dt class="font-semibold text-lg">I don"t have any good project ideas!</dt>
-        <dd class="mt-1 text-base">
-          Think about your interests or hobbies. If you like gaming, make a site about of some of the video games you like. If you"re into reading, make a site where users can post book reviews. The list goes on...
-        </dd>
-      </div>
-      <div>
-        <dt class="font-semibold text-lg">Are there example projects?</dt>
-        <dd class="mt-1 text-base">
-          I made a demo Flask project with CRUD on <a href="https://github.com/eesazahed/flask-movies" target="_blank" rel="noopener noreferrer" class="underline">GitHub</a>
-        </dd>
-      </div>
-    </dl>
-  </section>
+    <section class="pt-8 mb-16 max-w-3xl mx-auto" id="requirements">
+      <h2 class="text-3xl font-semibold mb-8">Requirements</h2>
+      <ul class="list-disc list-inside space-y-3 text-base">
+        <li>Build a Flask web app with at least 3 routes</li>
+        <li>Commit your code frequently on GitHub</li>
+        <li>Describe your project in a README.md file and include some screenshots</li>
+        <li>Host the app on Nest (deployment tutorial provided)</li>
+        <li>Implement basic CRUD functionality using SQLite, CSV, Postgres, or similar for higher rewards</li>
+        <li>Your project should be functional, somewhat styled, and easy to use</li>
+      </ul>
+      <p class="text-xl font-semibold mt-12">Additionally, please note that:</p>
+        <ul class="list-disc list-inside mt-6  space-y-3 text-base">
+        <li>You must be in high school or middle school</li>
+        <li>You started working on this project recently. No submitting old projects.</li>
+        <li>You haven't gotten a Slushies grant before. If you have, maybe try some other <a href="https://ysws.hackclub.com" target="_blank" rel="noopener noreferrer" class="underline">YSWS</a></li>
+        <li>This is a beginner YSWS, I don't want to see any AI made projects</li>
+        <li>If you do use AI a bit for inspiration/debugging, make sure to include it in your README</li>
+      </ul>
+    </section>
 
-    <section class="pt-8 mb-24 max-w-3xl mx-auto" id="faq">
-    <h2 class="text-3xl font-semibold mb-8">Guides</h2>
-    <dl class="space-y-8">
-      <div>
-        <dt class="font-semibold text-lg">Coming soon...</dt>
-        <dd class="mt-1 text-base">
-          Coming <a href="#" target="_blank" rel="noopener noreferrer" class="underline">soon</a>
-        </dd>
-      </div>
-    </dl>
-  </section>
+    <section class="pt-8 mb-16 max-w-3xl mx-auto" id="faq">
+      <h2 class="text-3xl font-semibold mb-8">Frequently Asked Questions</h2>
+      <dl class="space-y-8">
+        <div>
+          <dt class="font-semibold text-lg">Where can I ask for help?</dt>
+          <dd class="mt-1 text-base">
+            Join <a href="https://hackclub.slack.com" target="_blank" rel="noopener noreferrer" class="underline">#slushies</a> on Hack Club Slack or email <a href="mailto:eesa@hackclub.app" class="underline">eesa@hackclub.app</a>
+          </dd>
+        </div>
+        <div>
+          <dt class="font-semibold text-lg">I don"t have any good project ideas!</dt>
+          <dd class="mt-1 text-base">
+            Think about your interests or hobbies. If you like gaming, make a site about of some of the video games you like. If you"re into reading, make a site where users can post book reviews. The list goes on...
+          </dd>
+        </div>
+        <div>
+          <dt class="font-semibold text-lg">Are there example projects?</dt>
+          <dd class="mt-1 text-base">
+            I made a demo Flask project with CRUD on <a href="https://github.com/eesazahed/flask-movies" target="_blank" rel="noopener noreferrer" class="underline">GitHub</a>
+          </dd>
+        </div>
+      </dl>
+    </section>
 
-  <footer class="pt-8 text-sm text-center text-gray-600">
-    <p>
-      Questions? Join <a href="https://hackclub.slack.com" target="_blank" rel="noopener noreferrer" class="underline">#slushies</a> on Hack Club Slack or email 
-      <a href="mailto:eesa@hackclub.app" class="underline">eesa@hackclub.app</a>
-    </p>
-  </footer>
-</main>
+      <section class="pt-8 mb-16 max-w-3xl mx-auto" id="faq">
+      <h2 class="text-3xl font-semibold mb-8">Guides</h2>
+      <dl class="space-y-8">
+        <div>
+          <dt class="font-semibold text-lg">Coming soon...</dt>
+          <dd class="mt-1 text-base">
+            Coming <a href="#" target="_blank" rel="noopener noreferrer" class="underline">soon</a>
+          </dd>
+        </div>
+      </dl>
+    </section>
+
+    <footer class="pt-8 text-sm text-center text-gray-600">
+      <p>
+        Questions? Join <a href="https://hackclub.slack.com" target="_blank" rel="noopener noreferrer" class="underline">#slushies</a> on Hack Club Slack or email 
+        <a href="mailto:eesa@hackclub.app" class="underline">eesa@hackclub.app</a>
+      </p>
+    </footer>
+  </main>
+</div>
