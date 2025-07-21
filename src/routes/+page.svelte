283ec4d5be751aee1c33066ebf729d162a0b9f78
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-21 15:08:18 -0500

    edit site styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <title>Slushies, a Flask YSWS!</title>
 
-<main class="max-w-5xl mx-auto px-6 py-12">
-  <header class="mb-16 text-center">
+<header class="p-8 min-h-96 mb-16 text-center bg-linear-to-b from-lime-500 to-emerald-400 flex flex-col items-center justify-center">
+  <div class="text-white">
     <h1 class="text-5xl font-bold mb-4">Slushies</h1>
     <h2 class="text-2xl font-bold mb-4">A Flask YSWS</h2>
     <p class="text-lg max-w-3xl mx-auto">Build something really cool with Flask, ship your project, and get rewarded!</p>
-  </header>
+  </div>
+</header>
 
+<main class="max-w-5xl mx-auto px-24 pb-12">
   <section class="mb-16 grid grid-cols-1 md:grid-cols-2 gap-10 text-center">
-    <div class="p-6 border rounded">
+    <div class="p-12 border rounded-xl">
       <h2 class="text-3xl font-semibold mb-4">You Ship</h2>
       <p class="mb-2 text-base font-medium">A Flask web app with at least 3 routes. Host it on Nest.</p>
       <p class="text-sm">(don't worry I'll make some tutorials)</p>
     </div>
-    <div class="p-6 border rounded">
+    <div class="p-12 border rounded-xl">
       <h2 class="text-3xl font-semibold mb-4">We Ship</h2>
       <p class="mb-2 text-base font-medium">$5 reward for food vendors</p>
       <p class="text-sm">(Get $10 if you implement CRUD with SQLite, CSV, Postgres, or similar)</p>
@@ -22,11 +24,11 @@
 
   <section class="mb-16 text-center">
     <h2 class="text-3xl font-semibold mb-6">YSWS Programs</h2>
-    <p class="max-w-xl mx-auto mb-6 text-base">
+    <p class="max-w-xl mx-auto mb-9 text-base">
       Slushies is one of the many active YSWS programs where you build cool projects and get awesome rewards!
     </p>
     <a href="https://ysws.hackclub.com/" target="_blank" rel="noopener noreferrer"
-       class="inline-block px-6 py-3 border rounded hover:bg-gray-100 transition">
+       class="inline-block px-9 py-4 rounded-xl text-white bg-lime-600 hover:bg-lime-500 transition">
       Explore Programs →
     </a>
   </section>
@@ -36,7 +38,7 @@
     <ul class="list-disc list-inside space-y-3 text-base">
       <li>Build a Flask web app with at least 3 routes</li>
       <li>Commit your code frequently on GitHub</li>
-      <li>Talk about your project in a README.md file and include some screenshots</li>
+      <li>Describe your project in a README.md file and include some screenshots</li>
       <li>Host the app on Nest (deployment tutorial provided)</li>
       <li>Implement basic CRUD functionality using SQLite, CSV, Postgres, or similar for higher rewards</li>
       <li>Your project should be functional, somewhat styled, and easy to use</li>
