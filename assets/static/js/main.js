fe1e4f1c5634b97ff3599dd745645f042e6a38c5
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-31 23:39:34 -0500

    hackclub.com not .app

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -1,6 +1,6 @@
 if (window.location.hostname.includes("slushies.vercel.app")) {
   window.location.href = window.location.href.replace(
     "slushies.vercel.app",
-    "slushies.hackclub.app"
+    "slushies.hackclub.com"
   );
 }
